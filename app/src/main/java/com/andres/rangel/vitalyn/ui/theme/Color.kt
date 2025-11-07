02641b2da63e33ef8fe22b602eb3a4558e81d3b7
RangelJhoan <jhoanrangels2@gmail.com>
2025-11-07 15:51:26 -0500

    Add the application initial theme

--- app/src/main/java/com/andres/rangel/vitalyn/ui/theme/Color.kt
+++ app/src/main/java/com/andres/rangel/vitalyn/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.andres.rangel.vitalyn.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Base palette
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GrayLight = Color(0xFFCFE7CF)
+val Gray = Color(0xFF545454)
+
+val GreenPastel = Color(0xFFD7F4D7)
+val GreenDark = Color(0xFF050A05)
+
+val RedLight = Color(0xFFFFB4AB)
+val Red = Color(0xFF93000A)
+val RedDark = Color(0xFF690005)
