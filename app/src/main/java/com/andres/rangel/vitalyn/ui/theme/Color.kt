f43503650e505c5b432800d8ead753e4f8fd92a1
AndresRangel <andresrangel@MacBook-Pro-de-AndresRangel.local>
2025-11-09 18:07:50 -0500

    Update icons BottomNavigation and color theme

--- app/src/main/java/com/andres/rangel/vitalyn/ui/theme/Color.kt
+++ app/src/main/java/com/andres/rangel/vitalyn/ui/theme/Color.kt
@@ -5,11 +5,13 @@ import androidx.compose.ui.graphics.Color
 // Base palette
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
+val BlackBackground = Color(0xFF0A0A0A)
 
 val GrayLight = Color(0xFFF2F2F2)
 val Gray = Color(0xFF282D2D)
+val GrayDark = Color(0xFF151515)
 
-val GreenPastel = Color(0xFFCFE7CF)
+val GreenPastel = Color(0xFFB8E0B5)
 val GreenDark = Color(0xFF050A05)
 
 val RedLight = Color(0xFFFFB4AB)
