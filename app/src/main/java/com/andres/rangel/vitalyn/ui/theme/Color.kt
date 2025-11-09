b1bd5c7a8e1fad0e2ece13131573b903767ea701
RangelJhoan <jhoanrangels2@gmail.com>
2025-11-09 11:58:55 -0500

    Add an improvement to the app initial theme

--- app/src/main/java/com/andres/rangel/vitalyn/ui/theme/Color.kt
+++ app/src/main/java/com/andres/rangel/vitalyn/ui/theme/Color.kt
@@ -6,12 +6,12 @@ import androidx.compose.ui.graphics.Color
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 
-val GrayLight = Color(0xFFCFE7CF)
-val Gray = Color(0xFF545454)
+val GrayLight = Color(0xFFF2F2F2)
+val Gray = Color(0xFF282D2D)
 
-val GreenPastel = Color(0xFFD7F4D7)
+val GreenPastel = Color(0xFFCFE7CF)
 val GreenDark = Color(0xFF050A05)
 
 val RedLight = Color(0xFFFFB4AB)
-val Red = Color(0xFF93000A)
+val Red = Color(0xFF9F5050)
 val RedDark = Color(0xFF690005)
