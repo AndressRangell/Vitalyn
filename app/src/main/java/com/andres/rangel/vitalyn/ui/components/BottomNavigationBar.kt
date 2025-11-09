ba3667e5c980ec322c83a663dcad6fec509245a2
AndresRangel <andresrangel@MacBook-Pro-de-AndresRangel.local>
2025-11-09 18:07:50 -0500

    Update icons BottomNavigation and color theme

--- app/src/main/java/com/andres/rangel/vitalyn/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/andres/rangel/vitalyn/ui/components/BottomNavigationBar.kt
@@ -1,7 +1,14 @@
 package com.andres.rangel.vitalyn.ui.components
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,15 +18,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
@@ -29,12 +41,16 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import com.andres.rangel.vitalyn.R
 import com.andres.rangel.vitalyn.navigation.NavigationItem
 import com.andres.rangel.vitalyn.navigation.Screen
+import com.andres.rangel.vitalyn.ui.theme.GrayDark
+import com.andres.rangel.vitalyn.ui.theme.GrayLight
+import com.andres.rangel.vitalyn.ui.theme.GreenPastel
+import com.andres.rangel.vitalyn.ui.theme.RedDark
 
 @Composable
 fun BottomNavigationBar(
     navController: NavHostController,
     horizontalInset: Dp = 20.dp,
-    verticalInset: Dp = 20.dp,
+    verticalInset: Dp = 25.dp,
     cornerRadius: Dp = 35.dp
 ) {
     val currentDestination = navController.currentBackStackEntryAsState().value?.destination
@@ -49,7 +65,7 @@ fun BottomNavigationBar(
             shape = RoundedCornerShape(cornerRadius),
             tonalElevation = 8.dp,
             shadowElevation = 8.dp,
-            color = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
+            color = GrayDark,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(64.dp)
@@ -78,30 +94,44 @@ fun BottomNavigationBar(
 
 @Composable
 fun BottomNavigationItem(item: NavigationItem, selected: Boolean, onClick: () -> Unit) {
-    val scale by animateFloatAsState(
-        targetValue = if (selected) 1.20f else 1f,
-        animationSpec = spring(
-            dampingRatio = 0.5f,
-            stiffness = 300f
-        ),
-        label = "iconScaleAnim"
+    val backgroundColor by animateColorAsState(
+        targetValue = if (selected) GreenPastel.copy(alpha = 0.1f) else Color.Transparent,
+        animationSpec = tween(150, easing = LinearOutSlowInEasing),
+        label = "background animation"
     )
 
-    IconButton(
-        onClick = onClick,
-        modifier = Modifier.size(64.dp)
+    val scale by animateFloatAsState(
+        targetValue = if (selected) 1.15f else 1f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioNoBouncy,
+            stiffness = Spring.StiffnessHigh
+        ),
+        label = "scaleAnim"
+    )
+
+    Box(
+        modifier = Modifier
+            .size(64.dp)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) { onClick() },
+        contentAlignment = Alignment.Center
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .width(60.dp)
+                .height(55.dp)
+                .background(backgroundColor, shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
             Icon(
                 painter = painterResource(item.icon),
                 contentDescription = item.title,
                 modifier = Modifier
-                    .size(28.dp)
+                    .size(if (item.title == "Sports") 30.dp else 26.dp)
                     .graphicsLayer(scaleX = scale, scaleY = scale),
-                tint = if (selected)
-                    MaterialTheme.colorScheme.primary
-                else
-                    MaterialTheme.colorScheme.onSurfaceVariant
+                tint = if (selected) GreenPastel else GrayLight
             )
         }
     }
